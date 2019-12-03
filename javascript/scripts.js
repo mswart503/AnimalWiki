8d3fc6ccacd3ac879b877648ef55533f79c34b41
Elizabeth Van Hook and Matthew Swart <matthew@>
2019-12-02 20:50:47 -0800

    added pet test

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -15,4 +15,26 @@ $(document).ready(function() {
     $("#Penguin").hide();
     $("#Giraffe").hide();
   });
+
+  $("#btn").submit(function(event){
+    var cuddle = $("select#cuddlevirdict").val();
+    var age = parseInt($("input#age").val());
+    var pick = "";
+
+    if (cuddle === 'All' || cuddle === "Some" && age > 40){
+      pick = "Panda";
+    } else if (cuddle === "Never"){
+      pick = "Giraffe";
+    } else if (cuddle === 'All' || cuddle === "Some" && age <= 40){
+      pick = "Penguin";
+    } else {
+      pick = "something else"
+    }
+
+    $("#animal").empty().append(pick);
+    $("#results").show();
+
+
+    event.preventDefault();
+  });
 });
